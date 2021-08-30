bd67d27f6af61e23e13ea0aeb60c47716691b1c8
Tanmay Jain <tanmayjain@Tanmays-MacBook-Pro.local>
2021-08-30 18:27:10 +0530

    Removed false

--- fold1/fold2/tests/test_sample/test_file.py
+++ fold1/fold2/tests/test_sample/test_file.py
@@ -1,9 +1,10 @@
-from unittest.case import TestCase, main
+from unittest.case import TestCase
+from unittest import main
 
 class TestNothing(TestCase):
     def test_it(self):
         self.assertTrue(True)
-        self.assertTrue(False)
+        # self.assertTrue(False)
 
 if __name__=="__main__":
     main()
